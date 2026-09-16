c7afe658d7b566d8662120a4054ec124f1d32c1e
Julien <julienbh@gmail.com>
2026-09-16 19:27:43 -0400

    fix: avoid focusing lot search on open

--- src/components/shell/MobileLotSwitcher.ts
+++ src/components/shell/MobileLotSwitcher.ts
@@ -13,7 +13,6 @@ type MobileLotSwitcherContext = ShellPorts & MobileLotSwitcherState & {
   selectedLotItem: LotOptionItem | null;
   visibleLotItems: LotOptionItem[];
   closeLotSwitcher(): void;
-  focusLotSearch(): void;
   restoreLotSwitcherFocus(): void;
 };
 
@@ -45,13 +44,6 @@ export const MobileLotSwitcher = {
     }
   },
   methods: {
-    focusLotSearch(this: MobileLotSwitcherContext): void {
-      void nextTick(() => {
-        const field = this.$refs.lotSearchInput as { $el?: Element } | Element | undefined;
-        const root = field instanceof Element ? field : field?.$el;
-        root?.querySelector("input")?.focus();
-      });
-    },
     restoreLotSwitcherFocus(this: MobileLotSwitcherContext): void {
       void nextTick(() => {
         const trigger = this.$refs.lotSwitcherTrigger;
@@ -61,7 +53,6 @@ export const MobileLotSwitcher = {
     openLotSwitcher(this: MobileLotSwitcherContext): void {
       this.searchQuery = "";
       this.isOpen = true;
-      this.focusLotSearch();
     },
     closeLotSwitcher(this: MobileLotSwitcherContext): void {
       this.isOpen = false;
